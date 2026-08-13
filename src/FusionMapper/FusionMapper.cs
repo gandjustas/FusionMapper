aea55b56ac2cc8644495ff334a5fcaa9c09f3225
Stanislav Vyshchepan <me@stanislav-v.ru>
2026-08-13 03:16:50 +0300

    Prepare for generator

--- src/FusionMapper/FusionMapper.cs
+++ src/FusionMapper/FusionMapper.cs
@@ -1,61 +1,77 @@
 using System.Collections.Concurrent;
+using System.ComponentModel;
 using System.Linq.Expressions;
 
 namespace FusionMapper;
 
+#pragma warning disable S2955
 
 public static class FusionMapper
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public sealed class State
+    {
+        private State()
+        {
+        }
+
+        internal static readonly ConcurrentDictionary<(Type Source, Type Target), Delegate> MapDelegates = new();
+        internal static readonly ConcurrentDictionary<(Type Source, Type Target), Delegate> MapToExistingDelegates = new();
+        internal static readonly ConcurrentDictionary<(Type Source, Type Target), LambdaExpression> MapLambdaExpressions = new();
+
+    }
+
     public static FusionSource<TSource> Map<TSource>(this TSource source)
         => new(source);
 
     public static FusionProjection<TSource> Project<TSource>(this IQueryable<TSource> source)
         => new(source);
 
-    static readonly ConcurrentDictionary<(Type Source, Type Target), Delegate> MapDelegates = new();
-    static readonly ConcurrentDictionary<(Type Source, Type Target), Delegate> MapToExistingDelegates = new();
-    static readonly ConcurrentDictionary<(Type Source, Type Target), LambdaExpression> MapLambdaExpressions = new();
-
-    #pragma warning disable S2955
-
     internal static TTarget Map<TSource, TTarget>(TSource source)
     {
         Type sourceType = typeof(TSource);
         Type targetType = typeof(TTarget);
 
-        if (source == null && (targetType.IsClass ||  Nullable.GetUnderlyingType(targetType) != null))
+        if (source == null && (targetType.IsClass || Nullable.GetUnderlyingType(targetType) != null))
         {
             return default!;
         }
 
-        var func = (Func<TSource, TTarget>)MapDelegates.GetOrAdd((sourceType, targetType), 
-                _ => GetCreationLambda<TSource, TTarget>().Compile());
+        var func = (Func<TSource, TTarget>)State.MapDelegates.GetOrAdd(
+            (sourceType, targetType),
+            _ => GetCreationLambda<TSource, TTarget>().Compile());
+
         return func(source);
     }
 
     internal static TTarget Map<TSource, TTarget>(TSource source, TTarget target)
     {
-#pragma warning disable S2955
         if (source == null)
         {
             return target;
         }
-#pragma warning restore S2955
+
         ArgumentNullException.ThrowIfNull(target);
 
-        var del = MapToExistingDelegates.GetOrAdd((typeof(TSource), typeof(TTarget)), 
+        var del = State.MapToExistingDelegates.GetOrAdd(
+            (typeof(TSource), typeof(TTarget)),
             key => MappingBuilder.BuildAssignmentExpression(key.Source, key.Target).Compile());
+
         var action = (Action<TSource, TTarget>)del;
         action(source, target);
+
         return target;
     }
 
     internal static IQueryable<TTarget> Project<TSource, TTarget>(IQueryable<TSource> source)
     {
         ArgumentNullException.ThrowIfNull(source);
+
         var rewrittenSource = Rewrite(source);
+
         return rewrittenSource.Select(GetCreationLambda<TSource, TTarget>());
     }
+
     internal static IQueryable<TTarget> Rewrite<TTarget>(IQueryable<TTarget> query)
     {
         ArgumentNullException.ThrowIfNull(query);
@@ -75,13 +91,13 @@ public static class FusionMapper
         return query.Provider.CreateQuery<TTarget>(newExpression);
     }
 
-    static Expression<Func<TSource, TTarget>> GetCreationLambda<TSource, TTarget>() 
+    static Expression<Func<TSource, TTarget>> GetCreationLambda<TSource, TTarget>()
         => (Expression<Func<TSource, TTarget>>)GetCreationLambda(typeof(TSource), typeof(TTarget));
-    
-    internal static LambdaExpression GetCreationLambda(Type source, Type target) =>
-        MapLambdaExpressions.GetOrAdd((source, target),
-            key => MappingBuilder.BuildCreationLambda(key.Source, key.Target));    
 
+    internal static LambdaExpression GetCreationLambda(Type source, Type target) =>
+        State.MapLambdaExpressions.GetOrAdd(
+            (source, target),
+            key => MappingBuilder.BuildCreationLambda(key.Source, key.Target));
 }
 #pragma warning restore S2955
 
